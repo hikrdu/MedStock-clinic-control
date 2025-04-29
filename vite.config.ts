e1dd60e35c0ff6f0e0882278a6abbc6b14f4536d
Ricardo Nunes - Ext <ricardo.nunes.ext@brf.com>
2025-04-29 10:51:12 -0300

    refactor: comment out server configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    server: {
-        host: "::",
-        port: 8080,
-    },
+    // server: {
+    //     host: "::",
+    //     port: 8080,
+    // },
+    base: 'MedStock-clinic-control',
     plugins: [
         react(),
         mode === 'development' &&
