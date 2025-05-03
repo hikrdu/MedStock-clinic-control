4a2c08ba59e677bd537112b4812eee931dc669d0
Ricardo Silva <hikrdu@gmail.com>
2025-05-03 13:50:11 -0300

    Update productService.ts

--- src/services/api/productService.ts
+++ src/services/api/productService.ts
@@ -5,8 +5,8 @@ export const productService = {
     // Get all products ordered by name ascending
     getProducts: async (): Promise<Product[]> => {
         const products = await api.productApi.getProducts();
-        // return products.sort((a, b) => a.name.localeCompare(b.name));
-       return products;
+        return products.sort((a, b) => a.description.localeCompare(b.description));
+       // return products;
     },
 
     // Get a product by ID
